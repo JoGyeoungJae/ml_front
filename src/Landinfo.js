9bc617a0e1af256f81bcbdcaf01267b7ce54994a
leeos20k <leeos20k@naver.com>
2023-10-15 15:41:43 +0900

    디자인완성

--- src/Landinfo.js
+++ src/Landinfo.js
@@ -32,135 +32,153 @@ function Landinfo() {
   };
 
   return (
-    <div className="container">
-      {responseData ? (
-        <div className="content">
-          <div className="title-container">
-            <div className="title">
-              {language === "ko" ? responseData.nameKo : responseData.nameEn}
-            </div>
-            <div className="language-links">
-              <a onClick={toggleLanguage}>
-                {language === "ko" ? "English" : "한국어"}
-              </a>
-            </div>
-          </div>
-          <div className="main-image-container">
-            <img
-              className="main-image"
-              src={
-                process.env.PUBLIC_URL +
-                getImagePath(responseData.mainImage, responseData.mainImagePath)
-              }
-              alt=""
-            />
-          </div>
-          <div className="info-container">
-            <p className="info">
-              {language === "ko" ? "주소" : "Address"}:
-              {language === "ko"
-                ? responseData.addressKo
-                : responseData.addressEn}
-            </p>
-            <p className="info">
-              {language === "ko" ? "전화번호" : "Tel"}: {responseData.tel}
-            </p>
-          </div>
+    <div className="bigbox2">
+      <div className="box11">
+        <a onClick={toggleLanguage}>
+          {language === "ko" ? "Change to English" : "한글로 변경"}
+        </a>
+      </div>
+      <div className="box22"></div>
 
-          {/* 이미지 표시 */}
-          <div className="image-container">
-            {responseData.subimage1 && (
-              <div className="image-item-container">
-                <img
-                  className="image-item"
-                  src={
-                    process.env.PUBLIC_URL +
-                    getImagePath(
-                      responseData.subimage1,
-                      responseData.subImage1Path
-                    )
-                  }
-                  alt=""
-                />
-              </div>
-            )}
-            {responseData.subimage2 && (
-              <div className="image-item-container">
-                <img
-                  className="image-item"
-                  src={
-                    process.env.PUBLIC_URL +
-                    getImagePath(
-                      responseData.subimage2,
-                      responseData.subImage2Path
-                    )
-                  }
-                  alt=""
-                />
-              </div>
-            )}
-            {responseData.subimage3 && (
-              <div className="image-item-container">
-                <img
-                  className="image-item"
-                  src={
-                    process.env.PUBLIC_URL +
-                    getImagePath(
-                      responseData.subimage3,
-                      responseData.subImage3Path
-                    )
-                  }
-                  alt=""
-                />
-              </div>
-            )}
-            {responseData.subimage4 && (
-              <div className="image-item-container">
-                <img
-                  className="image-item"
-                  src={
-                    process.env.PUBLIC_URL +
-                    getImagePath(
-                      responseData.subimage4,
-                      responseData.subImage4Path
-                    )
-                  }
-                  alt=""
-                />
-              </div>
-            )}
-            {responseData.subimage5 && (
-              <div className="image-item-container">
-                <img
-                  className="image-item"
-                  src={
-                    process.env.PUBLIC_URL +
-                    getImagePath(
-                      responseData.subimage5,
-                      responseData.subImage5Path
-                    )
-                  }
-                  alt=""
-                />
-              </div>
-            )}
-          </div>
-          <div className="map-container">
+      <div className="box33">
+        <div className="container2">
+          {responseData ? (
             <div
-              dangerouslySetInnerHTML={{ __html: responseData.mapUrl }}
-            ></div>
-          </div>
+              className="content2"
+              style={{ maxHeight: "100%", overflowY: "scroll" }}
+            >
+              <div className="title-container">
+                <div className="title">
+                  {language === "ko"
+                    ? responseData.nameKo
+                    : responseData.nameEn}
+                </div>
+              </div>
+              <div className="main-image-container">
+                <img
+                  className="main-image"
+                  src={
+                    process.env.PUBLIC_URL +
+                    getImagePath(
+                      responseData.mainImage,
+                      responseData.mainImagePath
+                    )
+                  }
+                  alt=""
+                />
+              </div>
+              <div className="info-container">
+                <p className="info">
+                  {language === "ko" ? "주소" : "Address"}:
+                  {language === "ko"
+                    ? responseData.addressKo
+                    : responseData.addressEn}
+                </p>
+                <p className="info">
+                  {language === "ko" ? "전화번호" : "Tel"}: {responseData.tel}
+                </p>
+              </div>
 
-          {/* 설명 */}
-          <div className="description-container">
-            <p>
-              {language === "ko" ? responseData.linfoKo : responseData.linfoEn}
-            </p>
-          </div>
+              {/* 이미지 표시 */}
+              <div className="image-container">
+                {responseData.subimage1 && (
+                  <div className="image-item-container">
+                    <img
+                      className="image-item"
+                      src={
+                        process.env.PUBLIC_URL +
+                        getImagePath(
+                          responseData.subimage1,
+                          responseData.subImage1Path
+                        )
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+                {responseData.subimage2 && (
+                  <div className="image-item-container">
+                    <img
+                      className="image-item"
+                      src={
+                        process.env.PUBLIC_URL +
+                        getImagePath(
+                          responseData.subimage2,
+                          responseData.subImage2Path
+                        )
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+                {responseData.subimage3 && (
+                  <div className="image-item-container">
+                    <img
+                      className="image-item"
+                      src={
+                        process.env.PUBLIC_URL +
+                        getImagePath(
+                          responseData.subimage3,
+                          responseData.subImage3Path
+                        )
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+                {responseData.subimage4 && (
+                  <div className="image-item-container">
+                    <img
+                      className="image-item"
+                      src={
+                        process.env.PUBLIC_URL +
+                        getImagePath(
+                          responseData.subimage4,
+                          responseData.subImage4Path
+                        )
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+                {responseData.subimage5 && (
+                  <div className="image-item-container">
+                    <img
+                      className="image-item"
+                      src={
+                        process.env.PUBLIC_URL +
+                        getImagePath(
+                          responseData.subimage5,
+                          responseData.subImage5Path
+                        )
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="map-container">
+                <div
+                  dangerouslySetInnerHTML={{ __html: responseData.mapUrl }}
+                ></div>
+              </div>
+
+              {/* 설명 */}
+              <div className="description-container">
+                <p>
+                  {language === "ko"
+                    ? responseData.linfoKo
+                    : responseData.linfoEn}
+                </p>
+              </div>
+            </div>
+          ) : (
+            <p>{language === "ko" ? "데이터가 없습니다." : "NO Data"}</p>
+          )}
         </div>
-      ) : (
-        <p>{language === "ko" ? "데이터가 없습니다." : "NO Data"}</p>
-      )}
+      </div>
+      <div className="box44"></div>
+      <div className="box55"></div>
     </div>
   );
 }
